be3a3aa28499afa857278c9bd21e10015bfc869a
gokay <gokayacikgoz1@gmail.com>
2023-07-10 16:18:26 +0300

    Add project files.

---
+++ Casgem_Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblMessage.ToList();
+            return View(values);
+        }
+
+        public ActionResult MessageDelete(int id)
+        {
+            var values = db.TblMessage.Find(id);
+            db.TblMessage.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MessageDetails(int id)
+        {
+            var values = db.TblMessage.Find(id);
+            return View(values);
+        }
+    }
+}
