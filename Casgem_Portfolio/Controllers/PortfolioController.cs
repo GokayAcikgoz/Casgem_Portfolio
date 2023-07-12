c58cd8f24bc6e1c3b39ba26eac73072643bfcf01
gokay <gokayacikgoz1@gmail.com>
2023-07-12 09:29:16 +0300

    skill and portfolioController updated

---
+++ Casgem_Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar() 
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+
+            ViewBag.featureTitle = db.TblFeature.Select(x => x.FeatureTitle).FirstOrDefault();
+            ViewBag.featureDescription = db.TblFeature.Select(x => x.FeatureDescription).FirstOrDefault();
+            ViewBag.featureImage = db.TblFeature.Select(x => x.FeatureImageURL).FirstOrDefault();  
+
+            return PartialView();
+        }
+        
+        public PartialViewResult MyResume() 
+        {
+            var values = db.TblResume.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialStatistic()
+        {
+            ViewBag.totalService = db.TblService.Count();
+            ViewBag.totalMessage = db.TblMessage.Count();
+            ViewBag.totalThanksMessage = db.TblMessage.Where(x => x.MessageSubject == "Teşekkür").Count();
+            ViewBag.happyCustomer = 12;
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+
+            var textArray = db.TblSkill.Select(x => x.SkillName).ToArray();
+
+            ViewBag.skillArray = textArray;
+
+            return PartialView();
+        }
+
+
+        public PartialViewResult PartialWhoIAm()
+        {
+
+            ViewBag.title = db.TblWhoAmI.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = db.TblWhoAmI.Select(x => x.Description).FirstOrDefault();           
+            return PartialView();
+        }
+
+        public ActionResult Download()
+        {
+            ViewBag.cv = db.TblWhoAmI.Select(x => x.CvLink).FirstOrDefault();
+            string filePath = Server.MapPath("~/Templates/" + ViewBag.cv);
+
+            
+            return File(filePath, "application/pdf", ViewBag.cv);
+        }
+
+        public PartialViewResult PartialService()
+        {
+
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = db.TblTestimonial.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialVideo()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ Casgem_Portfolio/Controllers/StatisticController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.employeeCount = db.TblEmployee.Count();
+
+            var salary = db.TblEmployee.Max(x => x.EmployeeSalary);
+            ViewBag.maxSalaryEmployee = db.TblEmployee.Where(x => x.EmployeeSalary == salary).Select(
+                y => y.EmployeeName + " " + y.EmployeSurname).FirstOrDefault();
+
+
+            ViewBag.totalCityCount = db.TblEmployee.Select(x => x.EmployeeCity).Distinct().Count();
+
+            ViewBag.avgEmployeeSalary = db.TblEmployee.Average(x => x.EmployeeSalary);
+
+            ViewBag.countSoftwareDepartment = db.TblEmployee.Where(
+                x => x.EmployeeDepartment == db.TblDepartment.Where(
+                    z => z.DepartmentName == "Yazılım").Select(y => y.DepartmentID).FirstOrDefault()).Count();
+
+            ViewBag.cityAdanaOrAnkaraTotalSalary = db.TblEmployee.Where(
+                x => x.EmployeeCity == "Ankara" || x.EmployeeCity == "Adana").Sum(y => y.EmployeeSalary);
+
+            ViewBag.sumSalaryFromSoftwareDepartment = db.TblEmployee.Where(
+                x => x.EmployeeCity == "Ankara" && x.EmployeeDepartment == db.TblDepartment.Where(
+                    z => z.DepartmentName == "Yazılım").Select(
+                    y => y.DepartmentID).FirstOrDefault()).Sum(y => y.EmployeeSalary);
+
+            return View();
+        }
+    }
+}
